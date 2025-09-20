605f0d2547ae9ba72e40a31f8fe31ab30ce2b599
VrindaDevadas <vrinda03devadas@gmail.com>
2025-09-20 11:10:29 +0530

    changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,24 +4,25 @@ import Spline from '@splinetool/react-spline';
 
 const Hero: React.FC = () => {
     return (
-
         <section className="min-h-screen pb-20">
-            <div className="container mx-auto px-20">
+            <div className="container mx-auto px-11 lg:px-50">
 
                 <div className="flex flex-col lg:flex-row items-center justify-center gap-8 lg:gap-12">
 
 
-                    <div className="w-full max-w-lg lg:w-1/2 lg:max-w-none mx-auto aspect-square">
-                        <Spline scene="https://prod.spline.design/zPYoxgn8YjTIK4-R/scene.splinecode" />
+                    <div className="w-full max-w-md lg:w-1/2 lg:max-w-none mx-auto aspect-[4/5] overflow-hidden">
+                        <div className="w-[150%] h-full relative -translate-x-[15%] ">
+                            <Spline scene="https://prod.spline.design/zPYoxgn8YjTIK4-R/scene.splinecode" />
+                        </div>
                     </div>
 
                     {/* Text Content */}
-                    <div className="w-full lg:w-1/2 text-center lg:text-left">
-                        <h2 className="text-3xl font-bold text-brand-secondary-text">
+                    <div className="w-full lg:w-1/2 text-center lg:text-center pr-5 ">
+                        <h2 className="text-2xl font-bold text-brand-secondary-text ">
                             Hello There!
                         </h2>
 
-                        <h1 className="text-4xl md:text-5xl font-bold py-2 px-0 text-brand-primary-text my-3 whitespace-nowrap">
+                        <h1 className="text-3xl md:text-5xl font-bold text-brand-primary-text my-1.5">
                             I'm Vrinda Devadas
                         </h1>
                         <p className="text-lg text-brand-secondary-text leading-relaxed">
